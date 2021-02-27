a1ed8f6eb2493625ce63f9dc09458cb92d945fef
108061107 <amigo.159357@gmail.com>
2021-02-27 00:41:16 -0800

    add led3.cpp

---
+++ mbed-os-lab1-program/led3.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+
+
+void Led3(DigitalOut &ledpin)
+
+{
+
+   for (int i = 0; i < 6; ++i)
+
+   {                     //blink for 10 times
+
+      ledpin = !ledpin; // toggle led
+
+      ThisThread::sleep_for(100ms);
+
+   }
+
+}
